cb54ce5b9ad3a9563430b4e124a1c7cc4720dc6d
Mathew Chen <mathewchenpnw@gmail.com>
2022-03-24 23:44:19 -0700

    added width

--- pages/utility/wrapper.tsx
+++ pages/utility/wrapper.tsx
@@ -5,7 +5,7 @@ interface wrapperProps {}
 const Wrapper: React.FC<wrapperProps> = ({ children }) => {
   return (
     <div className="animate-fade flex justify-center">
-      <div className="max-w-lg p-3 w-1/2">{children}</div>
+      <div className="max-w-lg p-3 w-full">{children}</div>
     </div>
   );
 };
