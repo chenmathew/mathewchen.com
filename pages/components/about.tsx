906e8a30d9ddb5462435705dae469a17e8520f6c
Mathew Chen <mathewchenpnw@gmail.com>
2022-04-13 19:44:16 -0700

    updated me

--- pages/components/about.tsx
+++ pages/components/about.tsx
@@ -16,7 +16,7 @@ const About: React.FC<aboutProps> = ({}) => {
       <h1 className="font-light text-xl">What I Do</h1>
       <p className="indent-4 font-extralight">
         I&apos;m currently learning web development. Outside of programming,
-        I&apos;m interested in are trivia, video games, and working out. In the
+        I&apos;m interested in playing video games, working out, and finding new music to listen to. In the
         summers, I enjoy brewing my own kombucha.
       </p>
     </div>
